2e5e97edf1c4ef986a68c199bfcd9aabf9059d69
alan <alainedv2046@gmail.com>
2026-05-21 17:23:18 -0600

    Initialitation

--- front-end/capacitor.config.ts
+++ front-end/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'nails-study',
+  appName: 'front-end',
   webDir: 'www'
 };
 
